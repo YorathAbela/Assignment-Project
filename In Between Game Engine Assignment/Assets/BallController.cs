02f81512f4706d870a1b9be93ce68f3379056753
smushbush <74545738+smushbush@users.noreply.github.com>
2021-01-06 18:47:26 +0100

    AttemptCameraControl
    Some errors, will wait for feedback for fixing

---
+++ In Between Game Engine Assignment/Assets/BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    private bool ignoreNextCollision;
+
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     private void OnCollisionEnter2D(Collision2D other) {
+        
+        Debug.Log("Ball touched something");
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
