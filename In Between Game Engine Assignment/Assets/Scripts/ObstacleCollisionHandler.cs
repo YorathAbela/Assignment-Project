3144601f3f0f3414b7baa53544339ae583818317
YorathAbela <Yorath.Abela.e22117@mcast.edu.mt>
2021-01-06 15:50:47 +0100

    Ball & Platform Parenting
    Added platform and obstacles, parented platforms for optimisation

---
+++ In Between Game Engine Assignment/Assets/Scripts/ObstacleCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleCollisionHandler : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ball hit an Obstacle");
+        //reload the scene when ball hits an obstacle
+        SceneManager.LoadScene(0);
+    }
+}
