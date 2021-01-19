9b6b5a4b4b6e813bdbf73cdb1d1ea275fbeab72a
YorathAbela <Yorath.Abela.e22117@mcast.edu.mt>
2021-01-19 11:44:31 +0100

    Added CameraControl

---
+++ In Between Game Engine Assignment/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public BallController target;
+    private float offset;
+
+    // Awake method starts when camera Controller awakes
+    void Awake () {
+        Debug.Log("Awake was called");
+        offset = transform.position.y - target.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 curPos = transform.position;
+        curPos.y = target.transform.position.y + offset;
+        transform.position = curPos;
+    }
+}
