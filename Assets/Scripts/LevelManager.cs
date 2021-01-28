f2176ebb22eb0f3f22c208c77cf900b2e9f2c7f1
smushbush <74545738+smushbush@users.noreply.github.com>
2021-01-28 12:56:55 +0100

    Menu
    Menu was made.
    Replay and back button were also made.
    Back button made to go back to the main menu.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+
+        //outputs in the Console
+        print("Loading level " + name);
+
+        //loads the scene name
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGameEXE()
+    { 
+        Debug.Log ("Quit");
+        Application.Quit();
+    }
+
+    
+}
